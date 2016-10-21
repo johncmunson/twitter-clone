ec708fd371afbdb6219a4642c318028ba241fe2e
John Munson <johncmunson91@gmail.com>
2016-10-21 00:55:22 -0600

    prepped the files and added user stories. time to get coding!

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+var jq = $.noConflict();
+jq(document).ready(function(){
+
+    // TWEET MODULE
+
+    // Tweet button and char count hidden by default
+
+    // Tweet button and char count shown on text field focus
+
+    // Text field expands on focus
+
+    // Maximum of 140 chars in text field
+
+    // Char count to update dynamically
+
+    // Char count turn red when <= 10
+
+    // Tweet button adds tweet to feed on click and resets text field
+
+
+
+    // TWITTER FEED
+
+    // Tweet actions appear on hover
+
+    // Tweets are minimized by default
+
+    // Tweets expand on click
+
+    // Tweets minimize on defocus
+
+
+});
